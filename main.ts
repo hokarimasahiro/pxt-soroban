50c8a354fb458de67b0454ec8e6ad860d40c3f2f
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-01-18 05:14:24 +0900

    minus

--- main.ts
+++ main.ts
@@ -15,16 +15,24 @@ namespace soroban {
     //% blockId="show_soroban_number" block="show a number %n"
     //% weight=100 blockGap=8
     export function showNumber(n: number): void {
-        let wn=n
+        let wn=Math.abs(n)
         for(let i=4;i>=0;i--){
             let d=wn % 10
-            wn=Math.trunc(wn/10)
-            if(d>=5) led.plot(i,0); else led.unplot(i, 0);
-            d=d % 5
-            if(d>=4) led.plot(i, 1); else led.unplot(i, 1)
-            if (d >= 3) led.plot(i, 2); else led.unplot(i, 2)
-            if (d >= 2) led.plot(i, 3); else led.unplot(i, 3)
-            if (d >= 1) led.plot(i, 4); else led.unplot(i, 4)
+            if(wn==0){
+                led.unplot(i, 0)
+                led.unplot(i, 1)
+                if(n<=0) led.plot(i,2); else led.unplot(i,2);
+                led.unplot(i, 3)
+                led.unplot(i, 4)
+            }else{
+                if(d>=5) led.plot(i,0); else led.unplot(i, 0);
+                d=d % 5
+                if(d>=4) led.plot(i, 1); else led.unplot(i, 1)
+                if (d >= 3) led.plot(i, 2); else led.unplot(i, 2)
+                if (d >= 2) led.plot(i, 3); else led.unplot(i, 3)
+                if (d >= 1) led.plot(i, 4); else led.unplot(i, 4)
+                wn = Math.trunc(wn / 10)
+            }
         }
     }
 }
